ec188b4606768189e49a9b88ce9c02f236c6c5d3
Justin Lutz <jlutz1@leomail.tamuc.edu>
2022-11-11 17:48:02 -0600

    Solution for Hello World
    HelloWorld_C++

---
+++ helloWorld.cpp
@@ -0,0 +1,17 @@
+/**
+ * @file helloWorld.cpp
+ * @author Justin Lutz 
+ * @brief Solution to Kattis, Hello World!
+ * @version 0.1
+ * @date 2022-11-11
+ * 
+ * @about There is no input. The only output is "Hello World!"
+ * 
+ */
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!" << std::endl;
+}
