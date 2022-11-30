35fa8330b406e058664bef0fbb52fb2d642ad3a6
Justin Lutz <jlutz1@leomail.tamuc.edu>
2022-11-29 18:19:07 -0600

    Knights Packing Solution C++

---
+++ Easy Problems/KnightPacking.cpp
@@ -0,0 +1,25 @@
+/**
+ * @file KnightPacking.cpp
+ * @author Justin Lutz
+ * @brief NxN chess board. First player or second player that wins?
+ * @version 0.1
+ * @date 2022-11-29
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include <iostream>
+
+int main()
+{
+    // declare variable
+    int n;
+
+    // get input
+    std::cin >> n;
+
+    //use conditional operator
+    (n % 2 == 1) ? std::cout << "first" : std::cout << "second";
+
+}
