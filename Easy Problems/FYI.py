2aaceb3e6d28c8a4424e19caacc0693bdf790b3d
Justin Lutz <jlutz1@leomail.tamuc.edu>
2022-11-29 18:33:13 -0600

    FYI Python Solution

---
+++ Easy Problems/FYI.py
@@ -0,0 +1,17 @@
+
+# @file FYI.cpp
+# @author Justin Lutz
+# @brief Kattis Solution FYI. PYTHON SOLUTION.
+# @version 0.1
+# @date 2022-11-29
+# 
+# @copyright Copyright (c) 2022
+#
+#
+
+number = input()
+
+if number.startswith("555"):
+    print(1)
+else:
+    print(0)
