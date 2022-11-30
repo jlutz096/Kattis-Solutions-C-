dbafebe88b5941fbab5df95a216b1e69831baedd
Justin Lutz <jlutz1@leomail.tamuc.edu>
2022-11-29 18:26:37 -0600

    Solving for Carrots C++ Solution

---
+++ Easy Problems/SolvingForCarrots.cpp
@@ -0,0 +1,30 @@
+/**
+ * @file SolvingForCarrots.cpp
+ * @author Justin Lutz
+ * @brief  Kattis Problem Solving For Carrots.
+ * @version 0.1
+ * @date 2022-11-29
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include <iostream>
+
+int main()
+{
+    // declare variables
+    int x, y;
+
+    // declare string variable
+    std::string phrase;
+    
+    // get input
+    std::cin >> x >> y;
+    std::cin >> phrase;
+    
+    // print the number of problems solved
+    std::cout << y;
+
+    return 0;
+}
